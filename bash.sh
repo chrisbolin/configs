7a2a23ddb10ee184a2ce9b17fa5800607d02f7a0
Chris Bolin <bolin.chris@gmail.com>
2025-01-26 08:41:20 -0700

    add nvm

--- bash.sh
+++ bash.sh
@@ -12,7 +12,11 @@ defaults write com.apple.screencapture location "~/Documents/screenshots/";
 # osx aliases
 alias restartsound="sudo launchctl stop com.apple.audio.coreaudiod && sudo launchctl start com.apple.audio.coreaudiod"
 
+# nvm - node version manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # other aliases
 alias rmnodemodules='rm -r node_modules/'
 alias watch2='/Users/chris/repos/configs/utils/watch2'
-
